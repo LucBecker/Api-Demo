8e045247b81e321116c6425be30e0abb298cf3c9
Lucas Becker <lucas.andrade@riachuelo.com.br>
2023-05-13 23:37:25 -0300

    Create Component

--- src/main/java/br/com/projeto/ApiDemo/services/PessoaService.java
+++ src/main/java/br/com/projeto/ApiDemo/services/PessoaService.java
@@ -1,9 +1,12 @@
 package br.com.projeto.ApiDemo.services;
 
+import br.com.projeto.ApiDemo.model.Mensagem;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PessoaService {
 
-    
+    @Autowired
+    private Mensagem mensagem;
 }
